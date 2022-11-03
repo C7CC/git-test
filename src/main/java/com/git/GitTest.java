3e2fd518a140b6265a8183756d0f74ba477ffb4a
CCC <local@my.com>
2022-11-03 09:06:16 +0800

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("welcome to git");
         System.out.println("2 version");
+        System.out.println("3 version");
     }
 }
